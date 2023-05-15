34beb7de8ed232f5f8d6ac882d5a870fe0fb4270
Roger Arch <rogerfiblasalvado@gmail.com>
2023-05-15 16:59:58 +0200

    ezfac70r rust version 2

--- rust/target/debug/deps/factor-09c1dc8bddbd1f4a.d
+++
@@ -1,5 +0,0 @@
-/home/roger/Documents/CODING/RUST/factor/factor/target/debug/deps/factor-09c1dc8bddbd1f4a: src/main.rs
-
-/home/roger/Documents/CODING/RUST/factor/factor/target/debug/deps/factor-09c1dc8bddbd1f4a.d: src/main.rs
-
-src/main.rs:
--- rust/target/debug/factor.d
+++
@@ -1,1 +0,0 @@
-/home/roger/Documents/CODING/python/EzFAC70R/rust/target/debug/factor: /home/roger/Documents/CODING/python/EzFAC70R/rust/src/main.rs
