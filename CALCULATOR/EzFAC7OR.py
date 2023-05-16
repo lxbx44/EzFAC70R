d2f2c5865e967d75dd183b945f7cc2d1d21886f3
Roger Arch <rogerfiblasalvado@gmail.com>
2023-05-16 22:42:37 +0200

    added comments to calculator version

--- CALCULATOR/EzFAC7OR.py
+++ CALCULATOR/EzFAC7OR.py
@@ -6,26 +6,33 @@
 
 import math
 
+# Main title of the project
 def title():     
-    print("EzFAC7OR - Roger&Pep")
+    print("EzFAC7OR-Roger&Pep")
 
     
     
+# This function will mirror the final list 
+# for example: with input ->["1", "2", "7"], will return ["7", "2", "1"]
 def mirrorList(lst):
-
     mirrored = []
-    
+
     for i in range(len(lst)-1, -1, -1):
         mirrored.append(lst[i])
     
     return mirrored
 
+# Main function to factorize
 def numfact(n):
-    
+    # Empty list where all the factors will be appended
     factors = []
     
+    # Convert the value given to a float (to chech then for the easter eggs)
     n = float(n)
     
+    # Easter eggs
+    # 0, negative number, decimal number and 69 (for da jokes :P)
+
     if n == 0:
         print("What are you")
         print("doing silly?")
@@ -44,15 +51,20 @@ def numfact(n):
 
     ######################
 
+    # Actual factorizing
     else:
+        # Convert n to an integer (as n is a float)
         n = int(n)
         
+        # Adds to the empty list the number 1
         factors = [1]
         
+        # Adds all the 2s needed
         while n % 2 == 0:
             factors.append(2)
             n = n // 2
-            
+        
+        # Applies factorizing formula
         for i in range(3, int(math.sqrt(n))+1, 2):
             while n % i == 0:
                 factors.append(i)
@@ -61,15 +73,19 @@ def numfact(n):
         if n > 2:
             factors.append(n)
         
+        # The list gets mirrored (by the mirrorList() function)
         factors = mirrorList(factors)
         
+        # Finally all the elements of the list gets joined in a same string | from ["7", "2", "1"] to "7 2 1"
         print(' '.join(str(num) for num in factors))
         
     
 def main():
     
+    # Prints the main title
     title()
     
+    # Make a loop to factorize different numbers
     while True:
         print()
         print("Enter the number you")
@@ -87,8 +103,9 @@ def main():
         
 
 main()
-    
-    
+ 
+
+#    
 #   ;)
 #
 #   Roger&Pep
